fb3854e427e0826cf756d5a9bf123424c6f9eb50
basudev7240 <basudev7240@gmail.com>
2018-04-10 00:49:00 +0530

    Update GitDemoApplication.java
    dddddddddddd

--- gitDemo/src/main/java/in/test/gitDemo/GitDemoApplication.java
+++ gitDemo/src/main/java/in/test/gitDemo/GitDemoApplication.java
@@ -16,6 +16,6 @@ public class GitDemoApplication {
 	@RequestMapping("/api/rest/wish")
 	@GetMapping
 	public String welcome() {
-		return "hi....hello friends,welcome to git hub";
+		return "hi....hello friends,welcome to git hub basudev";
 	}
 }
